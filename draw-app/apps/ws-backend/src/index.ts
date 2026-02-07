7d70c098a71570ea2423334936a342f678d5cd06
Arun <hiarunpradhan@gmail.com>
2026-02-07 23:09:43 +0530

    turborepo project setup

---
+++ draw-app/apps/ws-backend/src/index.ts
@@ -0,0 +1,9 @@
+import { WebSocketServer } from "ws";
+
+let wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function message(data) {
+    ws.send("pong");
+  });
+});
