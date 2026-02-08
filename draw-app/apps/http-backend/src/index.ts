ae5956773287a4d6569078b067996f935672a88c
Arun <hiarunpradhan@gmail.com>
2026-02-08 22:00:29 +0530

    add zod-common package and use it

--- draw-app/apps/http-backend/src/index.ts
+++ draw-app/apps/http-backend/src/index.ts
@@ -2,6 +2,20 @@ import express from 'express'
 
 import {JWT_SECRET} from '@repo/backend-common/config';
 
+import {signInSchema,signUpSchema,webSocketSchema} from '@repo/common/zod'
+
+
 const app = express()
+
+app.post('/signin',async (req,res)=>{
+    const parsedContent = signUpSchema.safeParse(req.body)
+
+    if (!parsedContent.success) {
+        return res.status(411).json({message:'Invalid data',error:parsedContent.error})
+    }
+
+    
+})
+
 
 app.listen(3001)
---
+++ draw-app/packages/common/src/zod.ts
@@ -0,0 +1,19 @@
+import {z} from 'zod'
+
+const signUpSchema = z.object({
+    username:z.string().min(3),
+    password:z.string().min(8),
+    name:z.string().min(1)
+})
+
+const signInSchema = z.object({
+    username:z.string().min(3),
+    password:z.string().min(8)
+})
+
+const webSocketSchema = z.object({
+    name:z.string().min(1)
+})
+
+
+export {signInSchema,signUpSchema,webSocketSchema}
